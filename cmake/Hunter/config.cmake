77a33c7f3d2456346e9d1a4727cc8e5487b0186f
louis <louis@louis-MacBook-2.local>
2020-11-05 23:56:16 +0800

    fix: macos build error

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,2 +1,9 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
-hunter_config(Boost VERSION 1.66.0)
+# hunter_config(Boost VERSION 1.66.0)
+
+hunter_config(
+	Boost 
+	VERSION 1.70.0-p1
+	URL https://github.com/louis-tru/hunter-boost/archive/v1.70.0-p1.tar.gz
+	SHA1 54534240bb87c07a02ec53251d8ade1a57f0a3b0
+)
