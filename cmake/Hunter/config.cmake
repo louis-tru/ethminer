466555198122fe1b67708b0f4680b2dfd75d059e
louistru <louistru@hotmail.com>
2020-11-06 13:03:18 +0800

    fix: macos buiold error

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,7 +3,7 @@ hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_
 
 hunter_config(
 	Boost 
-	VERSION 1.70.0-p1
-	URL https://github.com/louis-tru/hunter-boost/archive/v1.70.0-p1.tar.gz
-	SHA1 54534240bb87c07a02ec53251d8ade1a57f0a3b0
+	VERSION 1.66.0-p1
+	URL https://github.com/louis-tru/hunter-boost/archive/v1.66.0-p1.tar.gz
+	SHA1 36638e4e595eaf9d20b4d28cb4e0857c7cd113ac
 )
